8311709447c8bbb814819d0f39e3e79a020e81d9
emmakuen <emmaku@protonmail.com>
2022-02-11 20:34:35 +0900

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button color="green" text="Add Task" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "App",
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="task">
+    <p>{{ task.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
