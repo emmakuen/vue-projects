f913e8d81b0aceeffe3e2d60e60cc986207ca697
emmakuen <emmaku@protonmail.com>
2022-02-12 13:07:50 +0900

    add maps

--- nuxt-airbnb-project/pages/home/_id.vue
+++ nuxt-airbnb-project/pages/home/_id.vue
@@ -18,21 +18,48 @@
     {{ home.reviewValue }} <br />
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
     {{ home.bathrooms }} bath <br />
+    <div style="height: 800px; width: 800px" ref="map"></div>
   </div>
 </template>
 
 <script>
 import homes from "~/data/homes";
+import secrets from "../../secrets";
 export default {
   head() {
     return {
       title: this.home.title,
+      script: [
+        {
+          src: `https://maps.googleapis.com/maps/api/js?key=${secrets.key}&libraries=places`,
+          hid: "map",
+          defer: true,
+        },
+      ],
     };
   },
   data() {
     return {
       home: {},
     };
+  },
+  mounted() {
+    const mapOptions = {
+      zoom: 18,
+      center: new window.google.maps.LatLng(
+        this.home._geoloc.lat,
+        this.home._geoloc.lng
+      ),
+      disableDefaultUI: true,
+      zoomControl: true,
+    };
+    const map = new window.google.maps.Map(this.$refs.map, mapOptions);
+    const position = new window.google.maps.LatLng(
+      this.home._geoloc.lat,
+      this.home._geoloc.lng
+    );
+    const marker = new window.google.maps.Marker({ position });
+    marker.setMap(map);
   },
   created() {
     const home = homes.find((home) => home.objectID == this.$route.params.id);
