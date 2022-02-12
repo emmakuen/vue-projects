7c87e0a0a155f78c8e3d1c49040b7009e63cbbe2
emmakuen <emmaku@protonmail.com>
2022-02-12 18:29:21 +0900

    add error page

---
+++ nuxt-airbnb-project/layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img
+      src="https://media1.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=790b76118284230a6b62a635762786c9957b27d6d8f27a3e&rid=giphy.gif&ct=g"
+      alt=""
+    /><br /><br />
+    <h1>Error!</h1>
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Error Occurred",
+    };
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- nuxt-airbnb-project/plugins/dataApi.js
+++ nuxt-airbnb-project/plugins/dataApi.js
@@ -5,15 +5,35 @@ export default function (context, inject) {
   });
   async function getHome(homeId) {
     const url = `https://${secrets.search_app_id}-dsn.algolia.net/1/indexes/homes/${homeId}`;
+    const headers = {
+      "X-Algolia-API-Key": secrets.search_api_key,
+      "X-Algolia-Application-ID": secrets.search_app_id,
+    };
 
-    const response = await fetch(url, {
-      headers: {
-        "X-Algolia-API-Key": secrets.search_api_key,
-        "X-Algolia-Application-ID": secrets.search_app_id,
-      },
-    });
+    try {
+      return unwrap(await fetch(url, { headers }));
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
 
+  async function unwrap(response) {
     const json = await response.json();
-    return json;
+    const { ok, status, statusText } = response;
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    };
+  }
+
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {},
+    };
   }
 }
