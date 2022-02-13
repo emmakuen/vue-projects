0deb86cc21db6dc07a43c80a43abc36870555212
emmakuen <emmaku@protonmail.com>
2022-02-14 01:25:14 +0900

    style map markers

--- nuxt-airbnb-project/pages/search.vue
+++ nuxt-airbnb-project/pages/search.vue
@@ -38,6 +38,7 @@ export default {
       return this.homes.map((home) => {
         return {
           ...home._geoloc,
+          pricePerNight: home.pricePerNight,
         };
       });
     },
@@ -67,4 +68,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.marker {
+  background-color: white;
+  border: 1px solid lightgray;
+  font-weight: bold;
+  border-radius: 20px;
+  padding: 5px 8px;
+}
+</style>
